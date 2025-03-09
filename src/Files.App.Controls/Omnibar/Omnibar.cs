957cc8927f16fc4200bd511cf348f3e673322d40
0x5BFA <62196528+0x5bfa@users.noreply.github.com>
2025-03-09 14:23:54 +0900

    Initialize Omnibar templates

---
+++ src/Files.App.Controls/Omnibar/Omnibar.cs
@@ -0,0 +1,60 @@
+﻿// Copyright (c) Files Community
+// Licensed under the MIT License.
+
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Markup;
+using Microsoft.UI.Xaml.Shapes;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.UI.Xaml.Input;
+
+namespace Files.App.Controls
+{
+	// Content
+	[ContentProperty(Name = nameof(Modes))]
+	// Template parts
+	[TemplatePart(Name = "ModesItemsRepeater", Type = typeof(ItemsRepeater))]
+	// Visual states
+	[TemplateVisualState(Name = "Focused", GroupName = "FocusStates")]
+	[TemplateVisualState(Name = "Unfocused", GroupName = "FocusStates")]
+	public partial class Omnibar : Control
+	{
+		private const string ModesItemsRepeater = "ModesItemsRepeater";
+
+		private ItemsRepeater? _modesItemsRepeater;
+
+		public Omnibar()
+		{
+			DefaultStyleKey = typeof(Omnibar);
+
+			Modes ??= [];
+		}
+
+		protected override void OnApplyTemplate()
+		{
+			_modesItemsRepeater = GetTemplateChild(ModesItemsRepeater) as ItemsRepeater
+				?? throw new MissingFieldException($"Could not find {ModesItemsRepeater} in {nameof(Omnibar)}'s style.");
+
+			_modesItemsRepeater.ItemsSource = Modes;
+
+			GotFocus += Omnibar_GotFocus;
+			LostFocus += Omnibar_LostFocus;
+
+			base.OnApplyTemplate();
+		}
+
+		private void Omnibar_GotFocus(object sender, RoutedEventArgs e)
+		{
+			VisualStateManager.GoToState(this, "Focused", true);
+		}
+
+		private void Omnibar_LostFocus(object sender, RoutedEventArgs e)
+		{
+			var focus = FocusState;
+
+			VisualStateManager.GoToState(this, "Unfocused", true);
+		}
+	}
+}
---
+++ src/Files.App.Controls/Omnibar/OmnibarMode.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) Files Community
+// Licensed under the MIT License.
+
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Markup;
+using Microsoft.UI.Xaml.Shapes;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Files.App.Controls
+{
+	public partial class OmnibarMode : Control
+	{
+		public OmnibarMode()
+		{
+			DefaultStyleKey = typeof(Omnibar);
+		}
+
+		protected override void OnApplyTemplate()
+		{
+			PointerEntered += OmnibarMode_PointerEntered;
+			PointerPressed += OmnibarMode_PointerPressed;
+			PointerReleased += OmnibarMode_PointerReleased;
+			PointerExited += OmnibarMode_PointerExited;
+
+			base.OnApplyTemplate();
+		}
+
+		private void OmnibarMode_PointerExited(object sender, PointerRoutedEventArgs e)
+		{
+			VisualStateManager.GoToState(this, "Normal", true);
+		}
+
+		private void OmnibarMode_PointerReleased(object sender, PointerRoutedEventArgs e)
+		{
+			VisualStateManager.GoToState(this, "PointerOver", true);
+		}
+
+		private void OmnibarMode_PointerPressed(object sender, PointerRoutedEventArgs e)
+		{
+			VisualStateManager.GoToState(this, "Pressed", true);
+		}
+
+		private void OmnibarMode_PointerEntered(object sender, PointerRoutedEventArgs e)
+		{
+			VisualStateManager.GoToState(this, "PointerOver", true);
+		}
+	}
+}
