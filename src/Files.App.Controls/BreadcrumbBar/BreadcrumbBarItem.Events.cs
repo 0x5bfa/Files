c21a8f37c0ba0aa5fb250d21f8ea044efc820cf6
0x5BFA <62196528+0x5bfa@users.noreply.github.com>
2025-03-17 03:16:20 +0900

    Initial commit

---
+++ src/Files.App.Controls/BreadcrumbBar/BreadcrumbBarItem.Events.cs
@@ -0,0 +1,88 @@
+﻿// Copyright (c) Files Community
+// Licensed under the MIT License.
+
+using Microsoft.UI;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Markup;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Shapes;
+
+namespace Files.App.Controls
+{
+	public partial class BreadcrumbBarItem
+	{
+		private void ItemButton_PointerEntered(object sender, PointerRoutedEventArgs e)
+		{
+			VisualStateManager.GoToState(this, "PointerOverItem", true);
+		}
+
+		private void ItemButton_PointerPressed(object sender, PointerRoutedEventArgs e)
+		{
+			VisualStateManager.GoToState(this, "PointerPressedOnItem", true);
+		}
+
+		private void ItemButton_PointerReleased(object sender, PointerRoutedEventArgs e)
+		{
+			OnItemClicked();
+
+			VisualStateManager.GoToState(this, "PointerOverItem", true);
+		}
+
+		private void ItemButton_PointerExited(object sender, PointerRoutedEventArgs e)
+		{
+			VisualStateManager.GoToState(this, "PointerNormal", true);
+		}
+
+		private void ChevronButton_PointerEntered(object sender, PointerRoutedEventArgs e)
+		{
+			VisualStateManager.GoToState(this, "PointerOverChevron", true);
+		}
+
+		private void ChevronButton_PointerPressed(object sender, PointerRoutedEventArgs e)
+		{
+			VisualStateManager.GoToState(this, "PointerPressedOnChevron", true);
+		}
+
+		private void ChevronButton_PointerReleased(object sender, PointerRoutedEventArgs e)
+		{
+			VisualStateManager.GoToState(this, "PointerOverChevron", true);
+
+			FlyoutBase.ShowAttachedFlyout(_itemChevronButton);
+		}
+
+		private void ChevronButton_PointerExited(object sender, PointerRoutedEventArgs e)
+		{
+			VisualStateManager.GoToState(this, "PointerNormal", true);
+		}
+
+		private void ChevronDropDownMenuFlyout_Opening(object? sender, object e)
+		{
+			if (_ownerRef is null ||
+				_ownerRef.TryGetTarget(out var breadcrumbBar) is false ||
+				sender is not MenuFlyout flyout)
+				return;
+
+			breadcrumbBar.RaiseItemDropDownFlyoutOpening(this, flyout);
+		}
+
+		private void ChevronDropDownMenuFlyout_Opened(object? sender, object e)
+		{
+			VisualStateManager.GoToState(this, "ChevronNormalOn", true);
+		}
+
+		private void ChevronDropDownMenuFlyout_Closed(object? sender, object e)
+		{
+			if (_ownerRef is null ||
+				_ownerRef.TryGetTarget(out var breadcrumbBar) is false ||
+				sender is not MenuFlyout flyout)
+				return;
+
+			breadcrumbBar.RaiseItemDropDownFlyoutClosed(this, flyout);
+
+			VisualStateManager.GoToState(this, "ChevronNormalOff", true);
+			VisualStateManager.GoToState(this, "PointerNormal", true);
+		}
+	}
+}
---
+++ src/Files.App.Controls/BreadcrumbBar/BreadcrumbBarItem.cs
@@ -0,0 +1,107 @@
+﻿// Copyright (c) Files Community
+// Licensed under the MIT License.
+
+using Microsoft.UI;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Markup;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Shapes;
+
+namespace Files.App.Controls
+{
+	public partial class BreadcrumbBarItem : ContentControl
+	{
+		// Constants
+
+		private const string TemplatePartName_ItemContentButton = "PART_ItemContentButton";
+		private const string TemplatePartName_ItemChevronButton = "PART_ItemChevronButton";
+		private const string TemplatePartName_ItemEllipsisDropDownMenuFlyout = "PART_ItemEllipsisDropDownMenuFlyout";
+		private const string TemplatePartName_ItemChevronDropDownMenuFlyout = "PART_ItemChevronDropDownMenuFlyout";
+
+		// Fields
+
+		private WeakReference<BreadcrumbBar>? _ownerRef;
+
+		private Border _itemContentButton = null!;
+		private Border _itemChevronButton = null!;
+		private MenuFlyout _itemEllipsisDropDownMenuFlyout = null!;
+		private MenuFlyout _itemChevronDropDownMenuFlyout = null!;
+
+		// Constructor
+
+		public BreadcrumbBarItem()
+		{
+			DefaultStyleKey = typeof(BreadcrumbBarItem);
+		}
+
+		// Methods
+
+		protected override void OnApplyTemplate()
+		{
+			base.OnApplyTemplate();
+
+			_itemContentButton = GetTemplateChild(TemplatePartName_ItemContentButton) as Border
+				?? throw new MissingFieldException($"Could not find {TemplatePartName_ItemContentButton} in the given {nameof(BreadcrumbBarItem)}'s style.");
+			_itemChevronButton = GetTemplateChild(TemplatePartName_ItemChevronButton) as Border
+				?? throw new MissingFieldException($"Could not find {TemplatePartName_ItemChevronButton} in the given {nameof(BreadcrumbBarItem)}'s style.");
+			_itemEllipsisDropDownMenuFlyout = GetTemplateChild(TemplatePartName_ItemEllipsisDropDownMenuFlyout) as MenuFlyout
+				?? throw new MissingFieldException($"Could not find {TemplatePartName_ItemEllipsisDropDownMenuFlyout} in the given {nameof(BreadcrumbBarItem)}'s style.");
+			_itemChevronDropDownMenuFlyout = GetTemplateChild(TemplatePartName_ItemChevronDropDownMenuFlyout) as MenuFlyout
+				?? throw new MissingFieldException($"Could not find {TemplatePartName_ItemChevronDropDownMenuFlyout} in the given {nameof(BreadcrumbBarItem)}'s style.");
+
+			if (IsEllipsis || IsLastItem)
+				VisualStateManager.GoToState(this, "ChevronCollapsed", true);
+
+			_itemContentButton.PointerEntered += ItemButton_PointerEntered;
+			_itemContentButton.PointerPressed += ItemButton_PointerPressed;
+			_itemContentButton.PointerReleased += ItemButton_PointerReleased;
+			_itemContentButton.PointerExited += ItemButton_PointerExited;
+
+			_itemChevronButton.PointerEntered += ChevronButton_PointerEntered;
+			_itemChevronButton.PointerPressed += ChevronButton_PointerPressed;
+			_itemChevronButton.PointerReleased += ChevronButton_PointerReleased;
+			_itemChevronButton.PointerExited += ChevronButton_PointerExited;
+
+			_itemChevronDropDownMenuFlyout.Opening += ChevronDropDownMenuFlyout_Opening;
+			_itemChevronDropDownMenuFlyout.Opened += ChevronDropDownMenuFlyout_Opened;
+			_itemChevronDropDownMenuFlyout.Closed += ChevronDropDownMenuFlyout_Closed;
+		}
+
+		public void OnItemClicked()
+		{
+			if (_ownerRef is not null &&
+				_ownerRef.TryGetTarget(out var breadcrumbBar))
+			{
+				if (IsEllipsis)
+				{
+					// Clear items in the ellipsis flyout
+					_itemEllipsisDropDownMenuFlyout.Items.Clear();
+
+					// Populate items in the ellipsis flyout
+					for (int index = 0; index < breadcrumbBar.IndexAfterEllipsis; index++)
+					{
+						if (breadcrumbBar.TryGetElement(index, out var item) && item?.Content is string text)
+						{
+							_itemEllipsisDropDownMenuFlyout.Items.Add(new MenuFlyoutItem() { Text = text });
+						}
+					}
+
+					// Open the ellipsis flyout
+					FlyoutBase.ShowAttachedFlyout(_itemContentButton);
+				}
+				else
+				{
+					// Fire a click event
+					breadcrumbBar.RaiseItemClickedEvent(this);
+				}
+			}
+		}
+
+		public void SetOwner(BreadcrumbBar breadcrumbBar)
+		{
+			_ownerRef = new(breadcrumbBar);
+		}
+	}
+}
