a3f2a7fc3d769cb82601611aa8d10a72d3c73514
0x5BFA <62196528+0x5bfa@users.noreply.github.com>
2025-04-10 11:49:26 +0900

    Init

---
+++ src/Files.App.Storage/Storables/HomeFolder/HomeFolder.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Files Community
+// Licensed under the MIT License.
+
+
+namespace Files.App.Storage.Storables
+{
+	public partial class HomeFolder : IHomeFolder
+	{
+		public string Id => throw new NotImplementedException();
+
+		public string Name => "Home";
+
+		public IAsyncEnumerable<IStorableChild> GetItemsAsync(StorableType type = StorableType.All, CancellationToken cancellationToken = default)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ src/Files.App.Storage/Storables/HomeFolder/IHomeFolder.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) Files Community
+// Licensed under the MIT License.
+
+namespace Files.App.Storage.Storables
+{
+	public partial interface IHomeFolder : IFolder
+	{
+	}
+}
