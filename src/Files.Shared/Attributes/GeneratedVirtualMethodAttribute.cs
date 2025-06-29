dd3286a39bae426fc1d096700e18a6ceaa054f53
0x5BFA <62196528+0x5bfa@users.noreply.github.com>
2025-06-29 13:15:05 +0900

    Init

---
+++ src/Files.Shared/Attributes/GeneratedVirtualMethodAttribute.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Files Community
+// Licensed under the MIT License.
+
+using System;
+
+namespace Files.Shared.Attributes;
+
+[AttributeUsage(AttributeTargets.Method, Inherited = false)]
+public class GeneratedVirtualMethodAttribute : Attribute
+{
+	public uint Index { get; init; } = 0U;
+}
