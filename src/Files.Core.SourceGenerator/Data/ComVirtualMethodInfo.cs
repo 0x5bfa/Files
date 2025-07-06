f317ca327cc05dbae11cca2843d9a0a173b829e2
0x5BFA <62196528+0x5bfa@users.noreply.github.com>
2025-07-06 11:56:07 +0900

    Add COM virtual method source generator

---
+++ src/Files.Core.SourceGenerator/Data/ComVirtualMethodInfo.cs
@@ -0,0 +1,7 @@
+﻿// Copyright (c) Files Community
+// Licensed under the MIT License.
+
+namespace Files.Core.SourceGenerator.Data
+{
+	internal record ComVirtualMethodInfo(string ReturnType, (string, string)[] Parameters);
+}
