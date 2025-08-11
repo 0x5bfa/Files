44e2c589b88ce3874ff3a425c28aa790eb827646
0x5BFA <62196528+0x5bfa@users.noreply.github.com>
2025-08-12 02:55:20 +0900

    Update

--- src/Files.App.Controls/ResizeVisual/ResizeVisual.cs
+++ src/Files.App.Controls/ResizeVisual/ResizeVisual.cs
@@ -1,35 +1,28 @@
 // Copyright (c) Files Community
 // Licensed under the MIT License.
 
-using CommunityToolkit.WinUI;
 using Microsoft.UI.Xaml.Input;
 
 namespace Files.App.Controls
 {
-	public sealed partial class ResizeVisual : Control
+	public partial class ResizeVisual : Control
 	{
-		/// <summary>Fires when the Thumb control loses mouse capture.</summary>
-		public event DragCompletedEventHandler? DragCompleted;
+		private Grid? _rootGrid;
+		private Thumb? _outerThumb;
 
-		/// <summary>Fires one or more times as the mouse pointer is moved when a Thumb control has logical focus and mouse capture.</summary>
-		public event DragDeltaEventHandler? DragDelta;
+		private bool _isDragging;
+		private bool _pointerExited;
+		private bool _reachedToBounds;
+		private double _deltaOutsideBounds;
 
 		/// <summary>Fires when a Thumb control receives logical focus and mouse capture.</summary>
 		public event DragStartedEventHandler? DragStarted;
 
-		private Grid? _root;
-		private Thumb? _thumb;
+		/// <summary>Fires one or more times as the mouse pointer is moved when a Thumb control has logical focus and mouse capture.</summary>
+		public event DragDeltaEventHandler? DragDelta;
 
-		private bool _pointerExited;
-
-		[GeneratedDependencyProperty]
-		public partial bool FollowPointer { get; set; }
-
-		[GeneratedDependencyProperty]
-		public partial double ThumbHeight { get; set; }
-
-		[GeneratedDependencyProperty]
-		public partial double ThumbWidth { get; set; }
+		/// <summary>Fires when the Thumb control loses mouse capture.</summary>
+		public event DragCompletedEventHandler? DragCompleted;
 
 		public ResizeVisual()
 		{
@@ -40,87 +33,44 @@ namespace Files.App.Controls
 		{
 			base.OnApplyTemplate();
 
-			_root = GetTemplateChild("PART_RootGrid") as Grid
+			_rootGrid = GetTemplateChild("PART_RootGrid") as Grid
 				?? throw new MissingFieldException($"Could not find {"PART_RootGrid"} in the given {nameof(ResizeVisual)}'s style.");
-			_thumb = GetTemplateChild("PART_Thumb") as Thumb
+			_outerThumb = GetTemplateChild("PART_Thumb") as Thumb
 				?? throw new MissingFieldException($"Could not find {"PART_Thumb"} in the given {nameof(ResizeVisual)}'s style.");
 
-			Unloaded += DragThumb_Unloaded;
+			Loaded += ResizeVisual_Loaded;
 
-			_root.PointerEntered += Grid_PointerEntered;
-			_root.PointerExited += Grid_PointerExited;
+			_rootGrid.PointerEntered += RootGrid_PointerEntered;
+			_rootGrid.PointerExited += RootGrid_PointerExited;
 
-			_thumb.PointerEntered += Thumb_PointerEntered;
-			_thumb.PointerExited += Thumb_PointerExited;
-			_thumb.DragStarted += Thumb_DragStarted;
-			_thumb.DragDelta += Thumb_DragDelta;
-			_thumb.DragCompleted += Thumb_DragCompleted;
+			_outerThumb.PointerEntered += OuterThumb_PointerEntered;
+			_outerThumb.PointerExited += OuterThumb_PointerExited;
+
+			_outerThumb.DragStarted += Thumb_DragStarted;
+			_outerThumb.DragDelta += Thumb_DragDelta;
+			_outerThumb.DragCompleted += Thumb_DragCompleted;
+
+			Unloaded += ResizeVisual_Unloaded;
 		}
 
-		private void DragThumb_Unloaded(object sender, RoutedEventArgs e)
+		protected static bool IsValidHeight(FrameworkElement target, double newHeight, double thisActualHeight)
 		{
-			if (_root is not null)
-			{
-				_root.PointerEntered -= Grid_PointerEntered;
-				_root.PointerExited -= Grid_PointerExited;
-			}
+			if (newHeight < (double.IsNaN(target.MinHeight) ? 0 : target.MinHeight) ||
+				newHeight > (double.IsNaN(target.MaxHeight) ? double.PositiveInfinity : target.MaxHeight) ||
+				newHeight <= thisActualHeight)
+				return false;
 
-			if (_thumb is not null)
-			{
-				_thumb.PointerEntered -= Thumb_PointerEntered;
-				_thumb.PointerExited -= Thumb_PointerExited;
-				_thumb.PointerExited -= Thumb_PointerExited;
-				_thumb.DragStarted -= Thumb_DragStarted;
-				_thumb.DragDelta -= Thumb_DragDelta;
-				_thumb.DragCompleted -= Thumb_DragCompleted;
-			}
+			return true;
 		}
 
-		private void Grid_PointerEntered(object sender, PointerRoutedEventArgs e)
+		protected static bool IsValidWidth(FrameworkElement target, double newWidth, double thisActualWidth)
 		{
-			if (_thumb is not null && !_thumb.IsDragging)
-			{
-				VisualStateManager.GoToState(this, "Visible", true);
-			}
-		}
+			if (newWidth < (double.IsNaN(target.MinWidth) ? 0 : target.MinWidth) ||
+				newWidth > (double.IsNaN(target.MaxWidth) ? double.PositiveInfinity : target.MaxWidth) ||
+				newWidth <= thisActualWidth)
+				return false;
 
-		private void Grid_PointerExited(object sender, PointerRoutedEventArgs e)
-		{
-			if (_thumb is not null && !_thumb.IsDragging)
-			{
-				VisualStateManager.GoToState(this, "Collapsed", true);
-			}
-		}
-
-		private void Thumb_PointerEntered(object sender, PointerRoutedEventArgs e)
-		{
-			_pointerExited = false;
-		}
-
-		private void Thumb_PointerExited(object sender, PointerRoutedEventArgs e)
-		{
-			_pointerExited = true;
-		}
-
-		private void Thumb_DragStarted(object sender, DragStartedEventArgs e)
-		{
-			DragStarted?.Invoke(this, e);
-		}
-
-		private void Thumb_DragDelta(object sender, DragDeltaEventArgs e)
-		{
-			DragDelta?.Invoke(this, e);
-		}
-
-		private void Thumb_DragCompleted(object sender, DragCompletedEventArgs e)
-		{
-			DragCompleted?.Invoke(this, e);
-
-			if (_thumb is not null && _pointerExited)
-			{
-				VisualStateManager.GoToState(this, "Collapsed", true);
-				_pointerExited = false;
-			}
+			return true;
 		}
 	}
 }
